eff792c8f850d9467b764a7aaaf720be46dd89f9
G Harish <151080900+gurugubelliharish@users.noreply.github.com>
2025-02-01 22:47:53 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ThreeDViewer from "./components/ThreeDViewer";
+
+function App() {
+  return (
+    <div>
+      <ThreeDViewer />
+    </div>
+  );
+}
+
+export default App;
